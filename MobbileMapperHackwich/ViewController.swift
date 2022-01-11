fe2bc1638910fa18b6f56076f9ada04ff44eee2f
Diego Gonzalez <45858@psdschool.org>
2022-01-11 11:57:16 -0700

    Slide #3 complete!

--- MobbileMapperHackwich/ViewController.swift
+++ MobbileMapperHackwich/ViewController.swift
@@ -5,15 +5,22 @@
 //  Created by Diego Gonzalez on 1/11/22.
 //
 
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    let locationManger = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.showsUserLocation = true
+        locationManger.requestWhenInUseAuthorization()
     }
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
 
 }
 
